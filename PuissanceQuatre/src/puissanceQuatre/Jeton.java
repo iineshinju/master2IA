c374aa8ea72e6e9c8319dcd13ad7d80a24903764
Laure <80780305+iineshinju@users.noreply.github.com>
2021-10-30 18:26:50 +0200

    add initialize of Jeton

--- PuissanceQuatre/src/puissanceQuatre/Jeton.java
+++ PuissanceQuatre/src/puissanceQuatre/Jeton.java
@@ -4,6 +4,18 @@ public class Jeton {
 
 	public Jeton() {
 		// TODO Auto-generated constructor stub
+		private Coordonnee place;
+		private boolean couleur; // true = couleur rouge, false = couleur jaune
+		
+		public Jeton(Coordonnee place) {
+			this.place = place;
+			// La valeur par défaut du boolean est false donc la couleur par défaut est jaune
+		}
+		
+		public Jeton(Coordonnee place, boolean couleur) {
+			this.place = place;
+			this.couleur = couleur;
+		}
 	}
 
 	public static void main(String[] args) {
