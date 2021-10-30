9ec02e7bb8db0a2c82c60872b0bc94dea4d276b1
Laure <80780305+iineshinju@users.noreply.github.com>
2021-10-30 19:15:29 +0200

    add testCoordonneeNotInRangeColonne()

--- PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
+++ PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
@@ -10,6 +10,15 @@ public class testCoordonnee {
 		}
 		return false; //Si on n'a pas attrapé l'exception c'est que les null sont accepté
 	}
+	
+	public boolean testCoordonneeNotInRangeColonne() {
+		try {
+			Coordonnee c = new Coordonnee(7, 1); // 7 est la 8e lettre de l'alphabet soit H, il ne fait pas partie de la grille
+		} catch (IllegalArgumentException)){
+			return true; //Si on a bien attrapé l'exception alors c'est vrai
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
