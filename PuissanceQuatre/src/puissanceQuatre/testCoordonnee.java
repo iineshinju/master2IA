d06b3e65fe9852274bd518ff921545a0cd3234d3
Laure <80780305+iineshinju@users.noreply.github.com>
2021-10-30 19:09:53 +0200

    add testLogneOrColonneNull()

--- PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
+++ PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
@@ -1,6 +1,15 @@
 package puissanceQuatre;
 
 public class testCoordonnee {
+	
+	public boolean testLigneOrColonneNull() {
+		try {
+			Coordonnee c = new Coordonnee(null, null);
+		} catch (NullPointerException)){
+			return true; //Si on a bien attrapé l'exception alors c'est vrai
+		}
+		return false; //Si on n'a pas attrapé l'exception c'est que les null sont accepté
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
