89e78b5a773db5af2ab8530d831a6c588b4eefc0
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-11-01 22:38:54 +0100

    CoordonneeConstructor1

--- PuissanceQuatre/src/puissanceQuatre/Coordonnee.java
+++ PuissanceQuatre/src/puissanceQuatre/Coordonnee.java
@@ -1,14 +1,43 @@
 package puissanceQuatre;
 
-public class Coordonnee {
+public class Coordonnee implements Comparable<Coordonnee> {
+	private int ligne; // La ligne est déterminée automatiquement
+	private int colonne;
 
-	public Coordonnee() {
-		// TODO Auto-generated constructor stub
+
+	public Coordonnee(int ligne, int colonne) {
+		
+	}
+	
+	public Coordonnee(String s) {
+		
+	}
+
+	public String toString() {
+		return "";
+	}
+	
+	public int getLigne() {
+		return 0;
+	}
+	
+	public int getColonne() {
+		return 0;
+	}
+	
+	public boolean equals(Object obj) {
+		return false;
+	}
+	
+	public boolean voisine(Coordonnee c) {
+		return false;
+	}
+	public int compareTo(Coordonnee o) {
+		return 0;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 	}
 
 }
