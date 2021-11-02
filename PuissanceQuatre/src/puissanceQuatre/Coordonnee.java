905d1865e6620b36e09987262a9ac9cad43ecfa1
Nour Sellidj <sellidj.nourelhouda@gmail.com>
2021-11-02 12:24:00 +0100

    CoordonneeConstructor2

--- PuissanceQuatre/src/puissanceQuatre/Coordonnee.java
+++ PuissanceQuatre/src/puissanceQuatre/Coordonnee.java
@@ -4,7 +4,9 @@ public class Coordonnee implements Comparable<Coordonnee> {
 	private int ligne; 
 	private int colonne;
 
-
+	public Coordonnee() {
+		throw new IllegalArgumentException("La coordonnée ne peut pas être null");
+	}
 	public Coordonnee(int ligne, int colonne) {
 		if(ligne < 0 || ligne > 5 || colonne < 0 || colonne > 6)
 			throw new IllegalArgumentException("La coordonnée (" + colonne +"," +ligne + ") est en dehors des indices valides");
@@ -13,7 +15,20 @@ public class Coordonnee implements Comparable<Coordonnee> {
 	}
 	
 	public Coordonnee(String s) {
-		
+		if (s.length() < 2) {
+			throw new IllegalArgumentException("La coordonnée n'est pas complète");
+		}
+		if (s.length() < 3) {
+			String lettre = s.substring(0, 1);
+			lettre = lettre.toUpperCase();
+			this.colonne = lettre.charAt(0) - 65;
+			this.ligne = Integer.parseInt(s.substring(1)) - 1;
+			if (this.colonne < 0 || this.colonne > 6 || this.ligne < 0 || this.ligne > 5)
+				throw new IllegalArgumentException("La coordonnée n'est pas valide");
+		}
+		else {
+			throw new IllegalArgumentException("La coordonnée " + this.colonne + this.ligne + " n'est pas valide");
+		}
 	}
 
 	public String toString() {
@@ -40,7 +55,8 @@ public class Coordonnee implements Comparable<Coordonnee> {
 	}
 
 	public static void main(String[] args) {
-		Coordonnee c = new Coordonnee(6,6);
+		//Coordonnee c = new Coordonnee(6,6);
+		Coordonnee c = new Coordonnee();
 	}
 
 }
