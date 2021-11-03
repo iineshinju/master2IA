ab9f81da31d2cfdc56bd41e013a25b3560bc4cc7
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 11:29:36 +0100

    add testCompareToColonne()

--- PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
+++ PuissanceQuatre/src/puissanceQuatre/testCoordonnee.java
@@ -91,6 +91,13 @@ public class testCoordonnee {
 		return compare == 1;
 	}
 	
+	public boolean testCompareToColonne() {
+		Coordonnee c = new Coordonnee(1,1);
+		Coordonnee d = new Coordonnee(2,0);
+		compare = c.compareTo(d);
+		return compare == -1;
+	}
+	
 	public boolean testEquals() {
 		Coordonnee c = new Coordonnee(0,0);
 		Coordonnee d = new Coordonnee(0,0);
