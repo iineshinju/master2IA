69edce01ae7a7fd12ecb19b5551aadfd01516dd3
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:59:40 +0100

    add testGetCouleur()

--- puissance4/src/puissance4/testJeton.java
+++ puissance4/src/puissance4/testJeton.java
@@ -8,9 +8,16 @@ public class testJeton {
 		return place;
 	}
 	
+	public static boolean testGetCouleur() {
+		Jeton j = new Jeton(new Coordonnee(0,0), true);
+		boolean couleur = j.getCouleur();
+		return couleur == true;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(testGetPlace());
+		System.out.println(testGetCouleur());
 	}
 
 }
