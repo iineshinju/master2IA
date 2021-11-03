3e1c94182524c526ad09936641808bacfa1b5c8d
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 13:07:46 +0100

    add testGetCouleur()

--- PuissanceQuatre/src/puissanceQuatre/testJeton.java
+++ PuissanceQuatre/src/puissanceQuatre/testJeton.java
@@ -8,6 +8,12 @@ public class testJeton {
 		return place;
 	}
 
+	public boolean testGetCouleur() {
+		Jeton j = new Jeton(new Coordonnee(0,0), true);
+		boolean couleur = j.getCouleur();
+		return couleur == true;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
