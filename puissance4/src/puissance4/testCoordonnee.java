ed76c428c28ccd7f611a8b3e3a251f9ce83da804
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:48:28 +0100

    add testCompareToEquals()

--- puissance4/src/puissance4/testCoordonnee.java
+++ puissance4/src/puissance4/testCoordonnee.java
@@ -77,6 +77,13 @@ public class testCoordonnee {
 		return colonne == 0;
 	}
 	
+	public static boolean testCompareToEquals() {
+		Coordonnee c = new Coordonnee(1,1);
+		Coordonnee d = new Coordonnee(1,1);
+		int compare = c.compareTo(d);
+		return compare == 0;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
