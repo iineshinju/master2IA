93e6a405f102d687a5c804d7cfb765c66185c7f2
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 15:00:16 +0100

    add testToStringRouge()

--- puissance4/src/puissance4/testJeton.java
+++ puissance4/src/puissance4/testJeton.java
@@ -14,10 +14,16 @@ public class testJeton {
 		return couleur == true;
 	}
 	
+	public static boolean testToStringRouge() {
+		Jeton j = new Jeton(new Coordonnee(0,0), true);
+		return "Les coordonnées sont : A1. La couleur du jeton est rouge." == j.toString(); 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(testGetPlace());
 		System.out.println(testGetCouleur());
+		System.out.println(testToStringRouge());
 	}
 
 }
