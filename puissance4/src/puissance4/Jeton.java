3490ff5a3070ec155d145c8c38cbfef738e688d0
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:36:48 +0100

    add constructor

--- puissance4/src/puissance4/Jeton.java
+++ puissance4/src/puissance4/Jeton.java
@@ -1,7 +1,9 @@
 package puissance4;
 
 public class Jeton {
-
+	private Coordonnee place;
+	private boolean couleur; // true = couleur rouge, false = couleur jaune
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
