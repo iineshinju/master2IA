de2c778339d5f4f1c3a87e9a4b200ca916ec7372
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:59:00 +0100

    add testGetPlace()

--- puissance4/src/puissance4/testJeton.java
+++ puissance4/src/puissance4/testJeton.java
@@ -2,9 +2,15 @@ package puissance4;
 
 public class testJeton {
 	
+	public static Coordonnee testGetPlace() {
+		Jeton j = new Jeton(new Coordonnee(0,0));
+		Coordonnee place = j.getPlace();
+		return place;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(testGetPlace());
 	}
 
 }
