a24c35c294a77ac03574632a784a32bc215552ba
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:38:33 +0100

    add Jeton(place)

--- puissance4/src/puissance4/Jeton.java
+++ puissance4/src/puissance4/Jeton.java
@@ -4,6 +4,11 @@ public class Jeton {
 	private Coordonnee place;
 	private boolean couleur; // true = couleur rouge, false = couleur jaune
 	
+	public Jeton(Coordonnee place) {
+		this.place = place;
+		// La valeur par défaut du boolean est false donc la couleur par défaut est jaune
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
