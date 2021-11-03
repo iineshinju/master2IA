f6e84d688e6615ea51163db34654c4d2a11ec094
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-03 14:43:25 +0100

    add testCoordonneeEmpty()

--- puissance4/src/puissance4/testCoordonnee.java
+++ puissance4/src/puissance4/testCoordonnee.java
@@ -2,6 +2,15 @@ package puissance4;
 
 public class testCoordonnee {
 	
+	public static boolean testCoordonneeEmpty() {
+		try {
+			Coordonnee c = new Coordonnee();
+		} catch (IllegalArgumentException e){
+			return true; //Si on a bien attrapé l'exception alors c'est vrai
+		}
+		return false; //Si on n'a pas attrapé l'exception c'est que les null sont accepté
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
