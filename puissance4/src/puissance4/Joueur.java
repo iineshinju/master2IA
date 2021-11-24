ec769299cb98b3082dc79bc5930e88babcfb3a4d
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-24 10:07:43 +0100

    add methods and constructors

--- puissance4/src/puissance4/Joueur.java
+++ puissance4/src/puissance4/Joueur.java
@@ -1,6 +1,55 @@
 package puissance4;
 
-public class Joueur {
+public abstract class Joueur {
+
+	public final static int HORIZONTALE = 1;
+	public final static int VERTICALE = 2;
+	public final static int DIAGONALE = 3;
+	public final static int EGALITE = 4;
+
+	private Coordonnee[] memoire; // memoire des placements des jetons d’une couleur
+
+	private Joueur adversaire;
+	private String nom;
+	private boolean couleurJ;
+
+	public Joueur(String nom, boolean couleurJ) {
+		this.nom = nom;
+		this.couleurJ = couleurJ;
+	}
+
+	public Joueur(boolean couleurJ) {
+		this.couleurJ = couleurJ;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void jouerAvec(Joueur j) {
+		// ....
+	deroulementJeu(this, j);
+	}
+
+	private static void deroulementJeu(Joueur placeur, Joueur observateur) {
+		int etat = 0;
+		int nbJetons = 0;
+		while (etat != HORIZONTALE || etat != VERTICALE || etat != DIAGONALE || etat != EGALITE) {
+			etat = placeur.choixPlacement();
+			nbJetons += 1;
+			if (nbJetons == 42)
+				etat = 4;
+			placeur.retourPlacement(etat);
+			observateur.retourObservation(etat);
+			Joueur x = placeur;
+			placeur = observateur;
+			observateur = x;
+		}
+	}
+	
+	protected abstract void retourPlacement(int etat);
+	protected abstract void retourObservation(int etat);
+	public abstract int choixPlacement();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
