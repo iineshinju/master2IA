418de5e6a5035306b48238e71c6e55179b27826f
Laure <80780305+iineshinju@users.noreply.github.com>
2021-11-25 11:06:30 +0100

    remove place in testJeton

--- puissance4/src/puissance4/testJeton.java
+++ puissance4/src/puissance4/testJeton.java
@@ -2,31 +2,24 @@ package puissance4;
 
 public class testJeton {
 	
-	public static boolean testGetPlace() {
-		Jeton j = new Jeton(new Coordonnee(0,0));
-		Coordonnee place = j.getPlace();
-		return place.toString().equals("A1");
-	}
-	
 	public static boolean testGetCouleur() {
-		Jeton j = new Jeton(new Coordonnee(0,0), true);
+		Jeton j = new Jeton(true);
 		boolean couleur = j.getCouleur();
-		return couleur == true;
+		return couleur;
 	}
 	
 	public static boolean testToStringRouge() {
-		Jeton j = new Jeton(new Coordonnee(0,0), true);
-		return "Les coordonnées sont : A1. La couleur du jeton est rouge.".equals(j.toString()); 
+		Jeton j = new Jeton(true);
+		return "La couleur du jeton est rouge.".equals(j.toString()); 
 	}
 	
 	public static boolean testToStringJaune() {
-		Jeton j = new Jeton(new Coordonnee(0,0));
-		return "Les coordonnées sont : A1. La couleur du jeton est jaune.".equals(j.toString()); 
+		Jeton j = new Jeton();
+		return "La couleur du jeton est jaune.".equals(j.toString()); 
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(testGetPlace());
 		System.out.println(testGetCouleur());
 		System.out.println(testToStringRouge());
 		System.out.println(testToStringJaune());
