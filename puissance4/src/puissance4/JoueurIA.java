2a0a1164e9a83323c37e2d62d04037c523aa0813
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-13 11:40:13 +0100

    add methods
    add voisinVide
    add choixPlacement

--- puissance4/src/puissance4/JoueurIA.java
+++ puissance4/src/puissance4/JoueurIA.java
@@ -1,5 +1,7 @@
 package puissance4;
 
+import java.lang.Math;   
+
 public class JoueurIA extends Joueur {
 	
 	protected Coordonnee[] position = new Coordonnee[21]; // memoire des placements de nos jetons
@@ -27,29 +29,129 @@ public class JoueurIA extends Joueur {
 		return indiceMax;
 	}
 	
-	public boolean positionNull() {
+	public boolean positionNull(Coordonnee[] tableau) {
 		int verification = 0;
-		for (int i = 0; i < this.position.length; i++) {
-			if (this.position[i] == null)
+		for (int i = 0; i < tableau.length; i++) {
+			if (tableau[i] == null)
 				verification++;
 		}
 		return verification == this.position.length;
 	}
 	
-	public int positionIndice() {
+	public int positionIndice(Coordonnee[] tableau) {
 		int verification = 0;
-		for (int i = 0; i < this.position.length; i++) {
-			if (this.position[i] != null)
+		for (int i = 0; i < tableau.length; i++) {
+			if (tableau[i] != null)
 				verification++;
 		}
 		return verification;
 	}
 	
+	public Coordonnee[] voisinVide(int indice) {
+		Coordonnee initial = this.position[indice-1];
+		int initLigne = initial.getLigne();
+		int initColonne = initial.getColonne();
+		Coordonnee[] vide = new Coordonnee[21];
+		
+		if (initColonne == 0) {
+			if (this.g.getLigne(initColonne) + 1 == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne), initColonne);
+
+			if (this.g.getLigne(initColonne + 1) == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1), initColonne +1);
+			else if (this.g.getLigne(initColonne + 1) + 1 == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1), initColonne +1);
+			else if (initLigne !=5 && this.g.getLigne(initColonne + 1) + 1 == initLigne + 1) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1) + 1, initColonne +1);
+				
+		} else if (initColonne == 6) {
+
+			if (this.g.getLigne(initColonne) + 1 == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne), initColonne);
+			
+			if (this.g.getLigne(initColonne - 1) == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1), initColonne -1);
+			else if (this.g.getLigne(initColonne - 1) + 1 == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1), initColonne -1);
+			else if (initLigne !=5 && this.g.getLigne(initColonne - 1) + 1 == initLigne + 1) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1) + 1, initColonne -1);
+		
+		} else {
+			if (this.g.getLigne(initColonne) + 1 == initLigne) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne), initColonne);
+			
+			if (this.g.getLigne(initColonne + 1) == initLigne) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1), initColonne +1);
+			else if (this.g.getLigne(initColonne + 1) + 1 == initLigne) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1), initColonne +1);
+			else if (initLigne !=5 && this.g.getLigne(initColonne + 1) + 1 == initLigne + 1) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne + 1) + 1, initColonne +1);
+			
+			if (this.g.getLigne(initColonne - 1) == initLigne) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1), initColonne -1);
+			else if (this.g.getLigne(initColonne - 1) + 1 == initLigne)
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1), initColonne -1);
+			else if (initLigne !=5 && this.g.getLigne(initColonne - 1) + 1 == initLigne + 1) 
+				vide[positionIndice(vide)] = new Coordonnee(this.g.getLigne(initColonne - 1) + 1, initColonne -1);
+		}
+		System.out.println("Vide : " + this.affichagetab(vide));
+		return vide;
+	}
+	
+	public Coordonnee[] concatenationVide(int indice) {
+		Coordonnee[] casesVide = new Coordonnee[42];
+		// PROBLEME DANS CETTE METHODE A CORRIGER
+		for (int i = 2; i < indice + 1; i++) {
+			Coordonnee[] intermediaire =  this.voisinVide(i);
+			System.out.println("concat-intermediaire : "+ this.affichagetab(intermediaire));
+			for (int j = 0; j < intermediaire.length; j++) {
+				if (intermediaire[j] != null)
+					casesVide[this.positionIndice(casesVide)] = intermediaire[j];
+			}
+		}
+		System.out.println("concatenation casesVide : "+ this.affichagetab(casesVide));
+		return casesVide;
+	}
+	
+	public String affichagetab(Coordonnee[] tab) {
+		String res = "[ ";
+		for (int i = 0 ; i < tab.length; i++) {
+			if (tab[i] != null)
+				res += tab[i] + ", ";
+		}
+		res += "]";
+		return res;
+	}
+	
 	@Override
 	public int choixPlacement() {//retour etat de ton jeu apres ton placement
+		int colonne;
+		int indice = this.positionIndice(this.position);
+		Coordonnee[] voisinVideIndice = null;
 		
-	   	System.out.println("Donner la colonne dans laquelle vous voulez mettre votre jeton : ");
+	   	if (this.positionNull(this.position)) {
+	   		colonne = this.poidsColonne();
+	   	} else {
+	   		if (indice == 1) {
+	   			voisinVideIndice = this.voisinVide(indice);
+	   		} else if (indice > 1){
+	   			voisinVideIndice = this.concatenationVide(indice);
+	   		} else {
+	   			colonne = this.poidsColonne();
+	   		}
+	   		int randomI = (int)(Math.random()*(this.positionIndice(voisinVideIndice)));
+	   		System.out.println("positionIndice : "+this.positionIndice(this.position));
+	   		System.out.println("voisinIndice : "+this.positionIndice(voisinVideIndice));
+	   		System.out.println("randomI : " + randomI);
+	   		colonne = this.positionNull(voisinVideIndice) ? this.poidsColonne() : voisinVideIndice[randomI].getColonne();
+	   		System.out.println("colonne : "+colonne);
+	   		System.out.println("voisinVide"+this.affichagetab(voisinVideIndice));
+	   	}
 		
+		this.g.ajouteJeton(this.jetonJ, colonne);
+		position[indice]= g.getDernierJeton();
+   		System.out.println("position : " + this.affichagetab(this.position));
+
 		System.out.println(this.g.toString());
 		
 		if(this.g.enHorizontale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
@@ -88,7 +190,12 @@ public class JoueurIA extends Joueur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Grille g = new Grille();
+		Jeton j = new Jeton();
+		Jeton r = new Jeton(true);
+		JoueurIA a = new JoueurIA("JoueurA", j, g);
+	   	JoueurGrille b = new JoueurGrille("JoueurB", r, g);
+	   	b.jouerAvec(a);		
 	}
 
 }
