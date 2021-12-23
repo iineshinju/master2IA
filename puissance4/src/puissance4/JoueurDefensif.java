c811a1d3861a214657f26a7a0df2aa71c2f78182
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-23 14:15:59 +0100

    correct error
    correct error with adversaire.getPosition()

--- puissance4/src/puissance4/JoueurDefensif.java
+++ puissance4/src/puissance4/JoueurDefensif.java
@@ -1,54 +1,65 @@
 package puissance4;
 
 public class JoueurDefensif extends JoueurIA {
-	
+
 	protected Coordonnee[] position = new Coordonnee[21];
 
 	public JoueurDefensif(String nom, Jeton jetonJ, Grille grilleJ) {
 		super(nom, jetonJ, grilleJ);
 	}
-	
+
 	public JoueurDefensif(Jeton jetonJ, Grille grilleJ) {
 		super(jetonJ, grilleJ);
 	}
-	
+
 	@Override
 	public int choixPlacement() {
-		this.position= adversaire.getPosition();
-		int colonne;
-		int indice = this.dernierIndiceTab(this.position);
+		Coordonnee[] adv = adversaire.getPosition();
+		int colonne = 0;
+		int indice = this.dernierIndiceTab(adv);
 		Coordonnee[] voisinVideIndice = null;
-		
-	   	if (this.tableauNull(this.position)) {
-	   		colonne = this.poidsColonne();
-	   	} else {
-	   		if (indice == 1) {
-	   			try {
-	   			System.out.println(this.voisinVide(indice));
-	   			voisinVideIndice = this.voisinVide(indice);
-	   			}
-	   			catch() {
-	   			}
-	   		} else if (indice > 1){
-	   			voisinVideIndice = this.concatenationVide(indice);
-	   		}
 
-	   		int[] valVoisin = new int[voisinVideIndice.length];
-	   		for(int i = 0; i < this.dernierIndiceTab(voisinVideIndice) ; i++) {
-	   			valVoisin[i] = verifVoisin(voisinVideIndice[i]);
-	   			
-	   		}
-	   		
-	   		colonne = voisinVideIndice[this.indiceMaxTab(valVoisin)].getColonne();
-	   	}
-		
+		if (this.tableauNull(adv)) {
+			colonne = this.poidsColonne();
+			System.out.println("Je suis dans le if");
+		} else {
+			System.out.println("Je suis dans le else");
+			try {
+				if (indice == 1) {
+					voisinVideIndice = this.voisinVide(indice, adv);
+					System.out.println("voisinVide 1 " + this.affichagetab(voisinVideIndice));
+
+				} else if (indice > 1) {
+					voisinVideIndice = this.concatenationVide(indice, adv);
+					System.out.println("voisinVide 1+" + this.affichagetab(voisinVideIndice));
+				}
+
+				int[] valVoisin = new int[voisinVideIndice.length];
+				for (int i = 0; i < this.dernierIndiceTab(voisinVideIndice); i++) {
+					valVoisin[i] = verifVoisin(voisinVideIndice[i], adv);
+					System.out.println("verifVoisin "+ i + " : "+ valVoisin[i]);
+				}
+				
+				colonne = voisinVideIndice[this.indiceMaxTab(valVoisin)].getColonne();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				int[] val = new int[7];
+				for (int j = 0; j < 7; j++) {
+					if (this.indiceJouable(j))
+						val[j] = 1;
+				}
+				for (int k = 0; k < val.length; k++) {
+					if (val[k] == 1)
+						colonne = k;
+				}
+			}
+		}
+
 		this.g.ajouteJeton(this.jetonJ, colonne);
-		position[indice]= g.getDernierJeton();
-   		System.out.println("position : " + this.affichagetab(this.position));
+		System.out.println("position : " + this.affichagetab(adv));
 
 		System.out.println(this.g.toString());
-		
-		if(this.g.enHorizontale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
+
+		if (this.g.enHorizontale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
 			return 1;
 		else if (this.g.enVerticale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
 			return 2;
@@ -56,9 +67,9 @@ public class JoueurDefensif extends JoueurIA {
 			return 3;
 		return 0;
 	}
-	
+
 	@Override
-	protected void retourPlacement(int etat) {//Retourne le message en fonction de l'etat
+	protected void retourPlacement(int etat) {// Retourne le message en fonction de l'etat
 		if (etat == 1)
 			System.out.println("Vous avez gagné en Verticale.");
 		else if (etat == 2)
@@ -67,11 +78,11 @@ public class JoueurDefensif extends JoueurIA {
 			System.out.println("Vous avez gagné en Diagonale.");
 		else if (etat == 4)
 			System.out.println("Egalit� !");
-		
+
 	}
 
 	@Override
-	protected void retourObservation(int etat) { //Retourne un message en fonction du jeu de l'adversaire
+	protected void retourObservation(int etat) { // Retourne un message en fonction du jeu de l'adversaire
 		if (etat == 1)
 			System.out.println("Vous avez perdu en Verticale.");
 		else if (etat == 2)
@@ -79,19 +90,16 @@ public class JoueurDefensif extends JoueurIA {
 		else if (etat == 3)
 			System.out.println("Vous avez perdu en Diagonale.");
 		else if (etat == 4)
-			System.out.println("Egalité !");
-	}	
-
-	
-	
+			System.out.println("Egalite !");
+	}
 
 	public static void main(String[] args) {
 		Grille g = new Grille();
 		Jeton j = new Jeton();
 		Jeton r = new Jeton(true);
-		JoueurIA a = new JoueurDefensif("JoueurA", j, g);
-	   	JoueurGrille b = new JoueurGrille("JoueurB", r, g);
-	   	b.jouerAvec(a);	
+		JoueurDefensif a = new JoueurDefensif("JoueurA Defensif", j, g);
+		JoueurGrille b = new JoueurGrille("JoueurB", r, g);
+		b.jouerAvec(a);
 
 	}
 
