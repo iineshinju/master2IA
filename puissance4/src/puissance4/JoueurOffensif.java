0ee55a973c3d36315734673f32ca60223ac84fa0
Laure <80780305+iineshinju@users.noreply.github.com>
2021-12-23 14:18:06 +0100

    add this.position where missing

--- puissance4/src/puissance4/JoueurOffensif.java
+++ puissance4/src/puissance4/JoueurOffensif.java
@@ -5,42 +5,54 @@ public class JoueurOffensif extends JoueurIA {
 	public JoueurOffensif(String nom, Jeton jetonJ, Grille grilleJ) {
 		super(nom, jetonJ, grilleJ);
 	}
-	
+
 	public JoueurOffensif(Jeton jetonJ, Grille grilleJ) {
 		super(jetonJ, grilleJ);
 	}
-	
+
 	@Override
 	public int choixPlacement() {
-		int colonne;
+		int colonne = 0;
 		int indice = this.dernierIndiceTab(this.position);
 		Coordonnee[] voisinVideIndice = null;
-		
-	   	if (this.tableauNull(this.position)) {
-	   		colonne = this.poidsColonne();
-	   	} else {
-	   		if (indice == 1) {
-	   			voisinVideIndice = this.voisinVide(indice);
-	   		} else if (indice > 1){
-	   			voisinVideIndice = this.concatenationVide(indice);
-	   		}
 
-	   		int[] valVoisin = new int[voisinVideIndice.length];
-	   		for(int i = 0; i < this.dernierIndiceTab(voisinVideIndice) ; i++) {
-	   			valVoisin[i] = verifVoisin(voisinVideIndice[i]);
-	   			
-	   		}
-	   		
-	   		colonne = voisinVideIndice[this.indiceMaxTab(valVoisin)].getColonne();
-	   	}
-		
+		if (this.tableauNull(this.position)) {
+			colonne = this.poidsColonne();
+		} else {
+			try {
+				if (indice == 1) {
+					voisinVideIndice = this.voisinVide(indice, this.position);
+				} else if (indice > 1) {
+					voisinVideIndice = this.concatenationVide(indice, this.position);
+				}
+
+				int[] valVoisin = new int[voisinVideIndice.length];
+				for (int i = 0; i < this.dernierIndiceTab(voisinVideIndice); i++) {
+					valVoisin[i] = verifVoisin(voisinVideIndice[i], this.position);
+				}
+				colonne = voisinVideIndice[this.indiceMaxTab(valVoisin)].getColonne();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				int[] val = new int[7];
+				for (int j = 0; j < 7; j++) {
+					if (this.indiceJouable(j))
+						val[j] = 1;
+				}
+				for (int k = 0; k < val.length; k++) {
+					if (val[k] == 1)
+						colonne = k;
+				}
+
+			}
+
+		}
+
 		this.g.ajouteJeton(this.jetonJ, colonne);
-		position[indice]= g.getDernierJeton();
-   		System.out.println("position : " + this.affichagetab(this.position));
+		position[indice] = g.getDernierJeton();
+		System.out.println("position : " + this.affichagetab(this.position));
 
 		System.out.println(this.g.toString());
-		
-		if(this.g.enHorizontale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
+
+		if (this.g.enHorizontale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
 			return 1;
 		else if (this.g.enVerticale(this.g.getDernierJeton(), this.jetonJ.getCouleur()))
 			return 2;
@@ -48,22 +60,22 @@ public class JoueurOffensif extends JoueurIA {
 			return 3;
 		return 0;
 	}
-	
+
 	@Override
-	protected void retourPlacement(int etat) {//Retourne le message en fonction de l'etat
+	protected void retourPlacement(int etat) {// Retourne le message en fonction de l'etat
 		if (etat == 1)
-			System.out.println("Vous avez gagné en Verticale.");
+			System.out.println("Vous avez gagne en Verticale.");
 		else if (etat == 2)
-			System.out.println("Vous avez gagné en Horizontale.");
+			System.out.println("Vous avez gagne en Horizontale.");
 		else if (etat == 3)
-			System.out.println("Vous avez gagné en Diagonale.");
+			System.out.println("Vous avez gagne en Diagonale.");
 		else if (etat == 4)
-			System.out.println("Egalit� !");
-		
+			System.out.println("Egalite !");
+
 	}
 
 	@Override
-	protected void retourObservation(int etat) { //Retourne un message en fonction du jeu de l'adversaire
+	protected void retourObservation(int etat) { // Retourne un message en fonction du jeu de l'adversaire
 		if (etat == 1)
 			System.out.println("Vous avez perdu en Verticale.");
 		else if (etat == 2)
@@ -71,19 +83,16 @@ public class JoueurOffensif extends JoueurIA {
 		else if (etat == 3)
 			System.out.println("Vous avez perdu en Diagonale.");
 		else if (etat == 4)
-			System.out.println("Egalité !");
-	}	
-
-	
-	
+			System.out.println("Egalite !");
+	}
 
 	public static void main(String[] args) {
 		Grille g = new Grille();
 		Jeton j = new Jeton();
 		Jeton r = new Jeton(true);
-		JoueurIA a = new JoueurOffensif("JoueurA", j, g);
-	   	JoueurGrille b = new JoueurGrille("JoueurB", r, g);
-	   	b.jouerAvec(a);	
+		JoueurOffensif a = new JoueurOffensif("JoueurA Offensif", j, g);
+		JoueurGrille b = new JoueurGrille("JoueurB", r, g);
+		b.jouerAvec(a);
 
 	}
 
